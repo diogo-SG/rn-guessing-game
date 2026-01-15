73274eaa277be1f57fe3ff6ee7557747b7919e1b
diogo-SG <diogo.carneiro@studiographene.com>
2026-01-15 15:55:01 +0000

    Refactor project structure by removing unused components and files, simplifying the layout, and updating the StartGameScreen with input functionality.

---
+++ app/index.tsx
@@ -0,0 +1,5 @@
+import StartGameScreen from "@/screens/StartGameScreen";
+
+export default function Index() {
+  return <StartGameScreen />;
+}
--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,11 +1,30 @@
-import { Text, View } from "react-native";
+import PrimaryButton from "@/components/PrimaryButton";
+import { StyleSheet, TextInput, View } from "react-native";
 
 function StartGameScreen() {
   return (
-    <View>
-      <Text>Start Game Screen</Text>
+    <View style={styles.inputContainer}>
+      <TextInput placeholder="Enter a number" />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#72063c",
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
